c038c4c3eb77d0a6b7da23865716485baaeea95a
bloopz@gmail.com <bloopz@gmail.com@e2f1a774-8aaf-70b2-72aa-2add4b3e3c02>
2012-03-21 17:23:32 +0000

    * Created the basic structure for the repo * All code resides in trunk/src, the reason for src/ and not trunk is   to be able to wrap around a netbeans project if you like to.

---
+++ trunk/trunk/src/se/etsf01/aesp/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.etsf01.aesp;
+
+/**
+ * The entry point of the AESP tool
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        System.out.println("AESP Main here!");
+    }
+}
